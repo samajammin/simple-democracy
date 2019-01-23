8b99eaa2aaead1ac9598b870e30503e6a6444584
Sam Richards <sbrichards@gmail.com>
2019-01-22 23:48:06 -0800

    Add openzeppelin-test-helpers

--- test/simple_democracy.test.js
+++ test/simple_democracy.test.js
@@ -1,4 +1,5 @@
 var SimpleDemocracy = artifacts.require('./SimpleDemocracy.sol');
+const { shouldFail } = require('openzeppelin-test-helpers');
 
 contract('SimpleDemocracy', function(accounts) {
   const creator = accounts[0];
@@ -7,15 +8,22 @@ contract('SimpleDemocracy', function(accounts) {
   const voter1 = accounts[3];
   const voter2 = accounts[4];
   const voter3 = accounts[5];
-  const voter4 = accounts[6];
 
   const candidate1 = accounts[7];
   const candidate2 = accounts[8];
   const candidate3 = accounts[9];
 
-  it('creator can add admins', async () => {
-    const democracy = await SimpleDemocracy.deployed();
+  const electionId = 1;
 
+  const invalidElectionId = 11;
+  const invalidCandidate = accounts[6];
+  const emptyAddress = '0x0000000000000000000000000000000000000000';
+
+  beforeEach(async () => {
+    this.democracy = await SimpleDemocracy.deployed();
+  });
+
+  it('creator can add admins', async () => {
     assert.equal(
       await democracy.getRegistration(creator),
       true,
@@ -53,8 +61,6 @@ contract('SimpleDemocracy', function(accounts) {
   });
 
   it('admin can add voters', async () => {
-    const democracy = await SimpleDemocracy.deployed();
-
     assert.equal(
       await democracy.getRegistration(voter1),
       false,
@@ -73,18 +79,24 @@ contract('SimpleDemocracy', function(accounts) {
       false,
       'voter1 should not be an admin'
     );
+
+    await shouldFail.reverting.withMessage(
+      democracy.addVoter(voter1, false, { from: admin1 }),
+      'Voter already registered.'
+    );
   });
 
-  xit('admin cannot add the same voter more than once', async () => {
-    // TODO raise exception
+  it('voters cannot add voters', async () => {
+    await shouldFail.reverting.withMessage(
+      democracy.addVoter(voter1, false, { from: voter1 }),
+      'Must be an admin.'
+    );
   });
 
   it('admin can add elections', async () => {
-    const democracy = await SimpleDemocracy.deployed();
     const candidates = [candidate1, candidate2, candidate3];
 
-    const electionId = 1;
-    await democracy.addElection(candidates);
+    await democracy.addElection(candidates, { from: admin1 });
     assert.equal(
       await democracy.getElectionIsOpen(electionId),
       true,
@@ -93,42 +105,65 @@ contract('SimpleDemocracy', function(accounts) {
   });
 
   it('voters can vote once in elections', async () => {
-    const democracy = await SimpleDemocracy.deployed();
-    const candidates = [candidate1, candidate2, candidate3];
-
-    const electionId = 1;
-    await democracy.addElection(candidates);
-
-    // vote first time success
     assert.equal(
       await democracy.getCandidateVoteCount(electionId, candidate2),
       1
     );
+
     await democracy.vote(electionId, candidate2, { from: voter1 });
+
     assert.equal(
       await democracy.getCandidateVoteCount(electionId, candidate2),
       2
     );
 
-    // vote second time raise exception
-    // await democracy.vote(electionId, candidate2, { from: voter1 });
-    // assert.equal(
-    //   await democracy.getCandidateVoteCount(electionId, candidate2),
-    //   2
-    // );
+    await shouldFail.reverting.withMessage(
+      democracy.vote(electionId, candidate2, { from: voter1 }),
+      'Already voted on this election.'
+    );
   });
 
-  xit('voters cannot vote in closed elections', async () => {
-    // TODO raise exception
+  it('unregistered voters cannot vote', async () => {
+    await shouldFail.reverting.withMessage(
+      democracy.vote(electionId, candidate1, { from: voter2 }),
+      'Must be a registered voter.'
+    );
   });
 
-  xit('voters cannot vote for candidates not in the election', async () => {
-    // TODO raise exception
+  it('voters cannot vote for candidates not in the election', async () => {
+    await shouldFail.reverting.withMessage(
+      democracy.vote(electionId, invalidCandidate, { from: voter1 }),
+      'Candidate is not in this election.'
+    );
   });
 
-  xit('voters cannot vote for candidates not in the election', async () => {
-    // TODO raise exception
+  it('only admin can close election & winner is established', async () => {
+    assert.equal(await democracy.getElectionIsOpen(electionId), true);
+    assert.equal(await democracy.getElectionWinner(electionId), emptyAddress);
+
+    await shouldFail.reverting.withMessage(
+      democracy.closeElection(electionId, { from: voter1 }),
+      'Must be an admin.'
+    );
+
+    await democracy.closeElection(electionId, { from: admin1 });
+
+    assert.equal(await democracy.getElectionIsOpen(electionId), false);
+    assert.equal(await democracy.getElectionWinner(electionId), candidate2);
   });
 
-  xit('admin can close election & winner is established', async () => {});
+  it('voters cannot vote in closed elections', async () => {
+    assert.equal(await democracy.getElectionIsOpen(electionId), false);
+    assert.equal(await democracy.getElectionIsOpen(invalidElectionId), false);
+
+    await shouldFail.reverting.withMessage(
+      democracy.vote(electionId, candidate1, { from: voter1 }),
+      'Election is closed.'
+    );
+
+    await shouldFail.reverting.withMessage(
+      democracy.vote(invalidElectionId, candidate1, { from: voter1 }),
+      'Election is closed.'
+    );
+  });
 });
