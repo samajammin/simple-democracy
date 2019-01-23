a3314b2130efcdedc1edac3ba4848b6e1607d68f
Sam Richards <sbrichards@gmail.com>
2019-01-22 21:35:30 -0800

    Add development network config

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 const SimpleDemocracy = artifacts.require('SimpleDemocracy');
+const SimpleStorage = artifacts.require('SimpleStorage');
+const TutorialToken = artifacts.require('TutorialToken');
+const ComplexStorage = artifacts.require('ComplexStorage');
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleDemocracy);
+  deployer.deploy(SimpleStorage); // TODO remove
+  deployer.deploy(TutorialToken); // TODO remove
+  deployer.deploy(ComplexStorage); // TODO remove
 };
